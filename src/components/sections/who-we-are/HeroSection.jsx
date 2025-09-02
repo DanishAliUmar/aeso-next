acc3fe3be5ac36315b9602ced3161f9094f24109
Danish Ali Umar <danishaliumar4@gmail.com>
2025-09-02 18:53:18 +0500

    fix section height issue

--- src/components/sections/who-we-are/HeroSection.jsx
+++ src/components/sections/who-we-are/HeroSection.jsx
@@ -9,7 +9,7 @@ export const HeroSection = ({ title, heading, image }) => {
             backgroundPosition: "center center",
         }}>
             <Header />
-            <div className="container mx-auto  px-4 sm:px-6 lg:px-8 flex items-end pb-28 min-h-[37.5rem]">
+            <div className="container mx-auto  px-4 sm:px-6 lg:px-8 flex items-end pb-28 min-h-[500px] sm:min-h-[37.5rem]">
                 <div className="space-y-4 pb-4">
                     <p className="text-xl md:text-2xl font-bold text-[#ffffffa6]">{title}</p>
                     <h2 className="text-5xl md:text-7xl leading-[1.2em]">{heading}</h2>
